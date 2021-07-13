3333ab8b0c421b8e815cfd326747cc440086cd09
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2021-07-13 21:53:48 +0200

    Added commands

--- main.c
+++ main.c
@@ -4,36 +4,40 @@
 
 #include "library.h"
 
-#define INPUT_SIZE 100
+#define MAX_FIRST_INPUT_SIZE 21
+#define MAX_COMMAND_INPUT_SIZE 16
+#define MAX_GRAPH_SIZE 1024
 
 int main(int argc, char *argv[]) {
 
     //Take the first line
-    char *input;
-    input = malloc(INPUT_SIZE * sizeof(char));
-    fgets(input, INPUT_SIZE, stdin);
+    char *firstInput;
+    firstInput = malloc(MAX_FIRST_INPUT_SIZE * sizeof(char));
+    fgets(firstInput, MAX_FIRST_INPUT_SIZE, stdin);
 
     long int nodesNumber = 0;
     long int rankingLength = 0;
 
-    takeFirstLine(input, &nodesNumber, &rankingLength);
+    takeFirstLine(firstInput, &nodesNumber, &rankingLength);
 
     fputs((const char *) nodesNumber, stdout);
     fputs("\n", stdout);
     fputs((const char *) rankingLength, stdout);
 
+    char *commandInput = malloc( MAX_COMMAND_INPUT_SIZE * sizeof(char) );
+    char *graphInput = malloc( MAX_GRAPH_SIZE * sizeof(char) );
+
     while(1){
-        fgets(input, INPUT_SIZE, stdin);
-        if(strcmp(input, "TopK") == 0){
+        fgets(commandInput, MAX_COMMAND_INPUT_SIZE, stdin);
+        if(strcmp(commandInput, "TopK\r\n") == 0){
             //return the result
             break;
         }
-        else {
-            //read the entire graph's adjacency matrix and execute the algorithm.
+        else if(strcmp(commandInput, "AggiungiGrafo\r\n") == 0) {
+            fputs("AggiungiGrafo\n", stdout);
+            fgets(graphInput, MAX_GRAPH_SIZE, stdin);
         }
     }
-
-    fgets(input, INPUT_SIZE, stdin);
 
     return 0;
 
