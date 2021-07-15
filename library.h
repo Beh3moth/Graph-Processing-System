fe62d3febef8abcab45d627e91fc8096b96a1910
Beh3moth <75836092+Beh3moth@users.noreply.github.com>
2021-07-15 20:01:12 +0200

    Everything should work except the Dijsktra alg

--- library.h
+++ library.h
@@ -1,10 +1,12 @@
 #ifndef UNTITLED_LIBRARY_H
 #define UNTITLED_LIBRARY_H
 
+#define INFINITE -1
+
 //TEXT MANAGEMENT METHODS
 
 /**
- * The method takes a line containing two numbers and puts the values in nodesNumber and in rankingLength.
+ * The method takes a line containing two numbers and puts the values in nodes and in rankingLength.
  * @param input is the input's first line.
  * @param nodesNumber is the number of nodes (long long int).
  * @param rankingLength is the length of the rank (long long int).
@@ -18,10 +20,10 @@ void takeFirstTwoNumbers(char * input, long long int * nodesNumber, long long in
 //MATRIX MANAGEMENT METHODS
 
 /**
- *
- * @param adjacencyMatrix
- * @param nodesNumber
- * @param line
+ * The function
+ * @param adjacencyMatrix is a pointer to the beginning of an array row.
+ * @param nodesNumber is the number of nodes of a graph.
+ * @param line is the line to put in the array.
  */
 void fillAdjacencyMatrix(long long int * adjacencyMatrix, long long int nodesNumber, char * line){
     for(int j=0; j<nodesNumber; j++){
@@ -31,5 +33,81 @@ void fillAdjacencyMatrix(long long int * adjacencyMatrix, long long int nodesNum
         *(adjacencyMatrix + j) = strtol(line, &line, 10);
     }
 }
+
+//DIJKSTRA MANAGEMENTS METHODS
+
+long long int nodes = 0;
+
+void setNodesNumber(long long int v){
+    nodes = v;
+}
+
+long long int minDistance(const long long int dist[nodes], const bool sptSet[nodes])
+{
+    // Initialize min value
+    long long int min = INFINITE;
+    long long int min_index = INFINITE;
+
+    for (int v = 0; v < nodes; v++) {
+        if (sptSet[v] == false && ((dist[v] <= min && dist[v]!=INFINITE) || min == INFINITE)) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+long long int dijkstra(long long int graph[nodes][nodes]){
+
+    //The output array. dist[i] will hold the shortest distance from src to i
+    long long int dist[nodes];
+
+    bool sptSet[nodes]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
+
+    // Initialize all distances as INFINITE and stpSet[] as false
+    for (long long int i = 0; i < nodes; i++){
+        dist[i] = INFINITE;
+        sptSet[i] = false;
+    }
+
+    // Distance of source vertex from itself is always 0
+    dist[0] = 0;
+
+    // Find shortest path for all vertices
+    for (long long int count = 0; count < nodes - 1; count++) {
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in the first iteration.
+        long long int u = minDistance(dist, sptSet);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for (int v = 0; v < nodes; v++)
+
+            // Update dist[v] only if is not in sptSet, there is an edge from
+            // u to v, and total weight of path from src to  v through u is
+            // smaller than current value of dist[v]
+            if (!sptSet[v] && graph[u][v] && dist[u] != INFINITE && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+    }
+
+    long long int distanceSum = 0;
+
+    for(int i=0; i<nodes; i++){
+        if(dist[i]!=INFINITE){
+            distanceSum += dist[i];
+        }
+    }
+
+    return  distanceSum;
+
+}
+
+//LINKED LIST MANAGEMENT METHODS
+
+
 
 #endif //UNTITLED_LIBRARY_H
--- main.c
+++ main.c
@@ -1,8 +1,77 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "library.h"
+
+typedef struct node {
+    long long int graphIndex;
+    long long int sumValue;
+    struct node * next;
+} node_t;
+
+void print_list(node_t * head, long long int rankingLength) {
+    node_t * current = head;
+    long long int counter = 0;
+    while (current != NULL && counter<rankingLength) {
+        printf("%lld ", current->graphIndex);
+        current = current->next;
+        counter++;
+    }
+}
+
+node_t* insertNode(node_t * head, long long int sumValue, long long int graphIndex, long long int rankingLength){
+
+    node_t *newNode, *prevNode, *nextNode;
+    node_t *headBackup;
+
+    newNode = (node_t *) malloc(sizeof(node_t));
+    newNode->graphIndex = graphIndex;
+    newNode->sumValue = sumValue;
+    newNode->next = NULL;
+
+    //Insert in head
+    if((head) == NULL){
+        (head) = newNode;
+    }
+    else {
+
+        nextNode = (head);
+        prevNode = NULL;
+        headBackup = (head);
+
+        long long int counter = 0;
+        while(nextNode != NULL && sumValue >= nextNode->sumValue && counter < rankingLength){
+            prevNode = nextNode;
+            counter++;
+            nextNode = nextNode->next;
+        }
+
+        if(counter<rankingLength){
+            //inserimento alla fine
+            if(nextNode == NULL){
+                prevNode->next = newNode;
+                //(head) = headBackup;
+            } else{
+                //inserimento in mezzo
+                if(prevNode != NULL) {
+                    newNode->next = prevNode->next;
+                    prevNode->next = newNode;
+                    //(head) = headBackup;
+                } else {
+                    //inserimento in testa
+                    newNode->next = (head);
+                    (head) = newNode;
+                }
+            }
+        }
+
+
+    }
+
+    return head;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -11,11 +80,15 @@ int main(int argc, char *argv[]) {
     long long int nodesNumber = 0;
     long long int rankingLength = 0;
     size_t len = 0;
+    node_t *head = NULL;
+
     //ssize_t lineSize = 0;
 
     getline(&input, &len, stdin);
 
     takeFirstTwoNumbers(input, &nodesNumber, &rankingLength);
+
+    setNodesNumber(nodesNumber);
 
     //Print d and k
     printf("%lld", nodesNumber);
@@ -25,26 +98,31 @@ int main(int argc, char *argv[]) {
 
     //Initialization of the adjacency Matrix
     long long int adjacencyMatrix[nodesNumber][nodesNumber];
+    long long int graphIndex = 0;
 
     //Command's cycle
     while(1){
 
         getline(&input, &len, stdin);
         if(strcmp(input, "TopK\r\n") == 0){
-            //return the result
+            fputs("TopK: ", stdout);
+            print_list(head, rankingLength);
             break;
         }
         else if(strcmp(input, "AggiungiGrafo\r\n") == 0) {
+
             fputs("AggiungiGrafo\n", stdout);
 
+            //Fill the adjacencyMatrix
             for(int i=0; i<nodesNumber; i++){
+                //The following line is just for debugging
                 if(getline(&input, &len, stdin)==-1){
                     printf("%s", "getLine failure");
                 }
                 fillAdjacencyMatrix(&adjacencyMatrix[i][0], nodesNumber, input);
             }
 
-
+            //Print the adjacencyMatrix
             for(int k=0; k<nodesNumber; k++){
                 for(int p=0; p<nodesNumber; p++){
                     printf("%lld", adjacencyMatrix[k][p]);
@@ -53,7 +131,12 @@ int main(int argc, char *argv[]) {
                 printf("%s", "\n");
             }
 
+            head = insertNode(head, dijkstra(adjacencyMatrix), graphIndex, rankingLength);
+            print_list(head, rankingLength);
+            graphIndex++;
+
         }
+
     }
 
     return 0;
